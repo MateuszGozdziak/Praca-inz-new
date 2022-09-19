2192b8d4c7adfdf1e4ec7c4fd8c4ba74ab47dd63
MateuszGoździak <mateuszgozd@gmail.com>
2022-09-19 18:43:28 +0200

    Add project files.

---
+++ WebApplication10/Entities/DataContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace WebApplication10.Models
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+
+        public DbSet<User> Users1 { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasKey(pc => new { pc.Id, pc.Name });
+
+        }
+    }
+}
